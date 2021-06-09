23a0e7f523f29e0347093f57600c02666d8f3b96
Shaw ODonnell <shawodonnell@googlemail.com>
2021-06-09 15:37:44 +0100

    new semi colon

--- src/route.js
+++ src/route.js
@@ -3,7 +3,7 @@ const path = require('path');
 const express = require('express');
 const hbs = require('hbs');
 const geocode = require('./utils/geocode.js');
-const forecast = require('./utils/forecast.js')
+const forecast = require('./utils/forecast.js');
 
 //EXPRESS AND PATH DECLARATIONS
 const app = express(); 
